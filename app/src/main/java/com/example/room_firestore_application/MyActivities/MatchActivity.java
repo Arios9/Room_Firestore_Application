80f895d4a5815681267deae7c5fd9c71b1b41e22
Arios <ariosssmfm@gmail.com>
2021-04-10 08:01:15 +0300

    MyActivities created

---
+++ app/src/main/java/com/example/room_firestore_application/MyActivities/MatchActivity.java
@@ -0,0 +1,16 @@
+package com.example.room_firestore_application.MyActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.room_firestore_application.R;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+}
