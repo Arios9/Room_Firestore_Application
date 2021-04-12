d2dd5205427d8b7812f3379e20fc2b49b501de6a
Arios <ariosssmfm@gmail.com>
2021-04-12 12:56:55 +0300

    changing id names to be unique

--- app/src/main/java/com/example/room_firestore_application/MyActivities/SportActivity.java
+++ app/src/main/java/com/example/room_firestore_application/MyActivities/SportActivity.java
@@ -3,10 +3,13 @@ package com.example.room_firestore_application.MyActivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.room_firestore_application.R;
 
 public class SportActivity extends AppCompatActivity {
+
+    private EditText name, gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
