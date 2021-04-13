c107ad5872ea6b7360c08b0cd6189f53abba15ab
Arios <ariosssmfm@gmail.com>
2021-04-13 13:44:53 +0300

    removing comments

--- app/src/main/java/com/example/room_firestore_application/ui/sport/SportFragment.java
+++ app/src/main/java/com/example/room_firestore_application/ui/sport/SportFragment.java
@@ -6,20 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.room_firestore_application.Local_Tables.Sport;
 import com.example.room_firestore_application.MainActivity;
 import com.example.room_firestore_application.R;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class SportFragment extends Fragment {
@@ -29,16 +24,9 @@ public class SportFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MainActivity.CurrentFragment = this;
-//        sportViewModel =
-//                new ViewModelProvider(this).get(SportViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_sport, container, false);
-//        final TextView textView = root.findViewById(R.id.text_sport);
-//        sportViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
         ListView lv = root.findViewById(R.id.sport_list);
         List<Sport> list = MainActivity.localDatabase.basicDao().getSport();
         List<String> strings = new ArrayList<>();
@@ -46,8 +34,6 @@ public class SportFragment extends Fragment {
             strings.add(sport.toString());
         }
 
-
-//        String []strings ={"C","C++","Java","Python","Go"};
         ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, strings);
         lv.setAdapter(arrayAdapter);
         return root;
