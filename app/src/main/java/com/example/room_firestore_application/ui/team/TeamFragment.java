9324970586342b8865162bb878f19206af16e5e4
Arios <ariosssmfm@gmail.com>
2021-04-13 17:17:31 +0300

    add delete for team

--- app/src/main/java/com/example/room_firestore_application/ui/team/TeamFragment.java
+++ app/src/main/java/com/example/room_firestore_application/ui/team/TeamFragment.java
@@ -4,34 +4,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.example.room_firestore_application.Local_Tables.Team;
 import com.example.room_firestore_application.MainActivity;
 import com.example.room_firestore_application.R;
-import java.util.ArrayList;
 import java.util.List;
 
 public class TeamFragment extends Fragment {
+
+    private ListView listView;
+    private List<Team> list;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MainActivity.CurrentFragment = this;
 
         View root = inflater.inflate(R.layout.fragment_team, container, false);
+        listView = (ListView) root.findViewById(R.id.team_list);
 
-        ListView lv = root.findViewById(R.id.team_list);
-        List<Team> list = MainActivity.localDatabase.basicDao().getTeam();
-        List<String> strings = new ArrayList<>();
-        for(Team team: list) {
-            strings.add(team.toString());
-        }
-
-        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, strings);
-        lv.setAdapter(arrayAdapter);
+        createList();
+        add_delete_listener(listView);
 
         return root;
     }
+
+
+    private void createList() {
+        list = MainActivity.localDatabase.basicDao().getTeam();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    private void add_delete_listener(ListView listView) {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Team team = (Team) parent.getItemAtPosition(position);
+                MainActivity.localDatabase.basicDao().delete(team);
+                Toast.makeText(getActivity(),"Deleted",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+
+
+
 }
